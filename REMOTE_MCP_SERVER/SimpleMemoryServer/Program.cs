f173a9c8f0793a0121be8a7dc4d951953bf49b3f
anpt1992 <anpt2009@outlook.com>
2025-07-19 09:23:22 +0700

    Implement remote mcp

--- REMOTE_MCP_SERVER/SimpleMemoryServer/Program.cs
+++ REMOTE_MCP_SERVER/SimpleMemoryServer/Program.cs
@@ -1,5 +1,18 @@
+
+using ModelContextProtocol.AspNetCore;
+using Tools;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register MCP server and discover tools from the current assembly
+builder.Services.AddMcpServer()
+    .WithHttpTransport()
+    .WithToolsFromAssembly();
+
 var app = builder.Build();
+
+// Add MCP middleware
+app.MapMcp();
 
 app.MapGet("/", () => "Hello World!");
 
