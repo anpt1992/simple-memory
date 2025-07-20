68f8e9158d25e05fdf21b6cc249777eb2dc55142
anpt1992 <anpt2009@outlook.com>
2025-07-20 17:38:21 +0000

    Add initial implementation of SimpleMemoryServer with Docker support, configuration files, and memory tools

---
+++ SimpleMemoryServer/Tools/GreetingTools.cs
@@ -0,0 +1,17 @@
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace Tools
+{
+    [McpServerToolType]
+    public sealed class GreetingTools
+    {
+        public GreetingTools() { }
+
+        [McpServerTool, Description("Says Hello to a user")]
+        public static string Echo(string username)
+        {
+            return "Hello " + username;
+        }
+    }
+}
